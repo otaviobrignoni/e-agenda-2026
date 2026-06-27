fa99eec2c262a18747df7bf0108fa845fc921862
otaviobrignoni <otavio.678.b@gmail.com>
2026-06-27 18:30:03 -0300

    altera noma de parametro na entidade base para seguir padrões do projeto

--- eAgenda.WebApp/Compartilhado/ModuloBase/EntidadeBase.cs
+++ eAgenda.WebApp/Compartilhado/ModuloBase/EntidadeBase.cs
@@ -3,5 +3,5 @@ namespace eAgenda.WebApp.Compartilhado.ModuloBase;
 public abstract class EntidadeBase<T> where T : EntidadeBase<T>
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public abstract void Atualizar(T entidadeAtualizada);
+    public abstract void Atualizar(T registroEditado);
 }
